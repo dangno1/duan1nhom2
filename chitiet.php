3e9c0831c3ca5313a6eaf25def04aa1b46e7bdd5
Dang Nd <97234801+dangno1@users.noreply.github.com>
2022-11-22 02:47:02 +0700

    Sua Bai

--- chitiet.php
+++ chitiet.php
@@ -2,22 +2,23 @@
     session_start();
     require('./model/connect.php');
     require('./model/roomBook.php');
-    if(!empty($_GET['room_id'])) {
-        $id_room = $_GET['room_id'];
+    if(!empty($_GET['kind_of_room_id'])) {
 
-        $sql = "SELECT * FROM `room` WHERE `room_id` = $id_room";
+        $kind_of_room_id = $_GET['kind_of_room_id'];
+
+        $sql = "SELECT * FROM `kindRoom` WHERE `kind_of_room_id` = $kind_of_room_id";
         $result = $connect->query($sql);
         $result->execute();
-        $room = $result->fetch();
+        $kindRoom = $result->fetch();
 
         // anh room
-        $sql_img = "SELECT * FROM `roomImage` WHERE `room_id` = $id_room";
+        $sql_img = "SELECT * FROM `roomImage` WHERE `kind_of_room_id` = $kind_of_room_id";
         $result = $connect->query($sql_img);
         $result->execute();
         $room_img = $result->fetchAll();
 
         //get comment
-        $commentSql = "SELECT comment.content_comment, user.name_user FROM comment INNER JOIN user ON comment.user_id = user.user_id WHERE comment.room_id = $id_room";
+        $commentSql = "SELECT comment.content_comment, user.name_user FROM comment INNER JOIN user ON comment.user_id = user.user_id WHERE comment.room_id = $kind_of_room_id";
         $result = $connect->query($commentSql);
         $result->execute();
         $comments = $result->fetchAll();
@@ -39,22 +40,22 @@
     <div class="container">
         <?php require('./view/header.php'); ?>
         <?php 
-            if(!empty($room)) {
+            if(!empty($kindRoom)) {
         ?>
         <div class="deitail-room">
             <div class="tile">
                 <span>Big Hotel</span>
-                <h2><?php echo $room['kind_of_room'] ?></h2>
+                <h2><?php echo $kindRoom['kind_of_room'] ?></h2>
             </div>
             <div class="banner">
                 <div class="banner-left">
-                    <img src="./controller/room/<?php echo $room['image_room'] ?>" width="1024px" height="683px">
+                    <img src="./controller/kindRoom/<?php echo $kindRoom['image'] ?>" width="1024px" height="683px">
                 </div>
                     <div class="banner-right">
                     <?php 
                         foreach ($room_img as $item) {
                     ?>
-                        <img src="./controller/room/<?php echo $item['image_room'] ?>" width="100%" height="335px">
+                        <img src="./controller/kindRoom/<?php echo $item['image_room'] ?>" width="100%" height="335px">
                     <?php 
                         }
                     ?>
@@ -65,7 +66,7 @@
         <div class="produc">
             <div class="room-desc">
                 <h3>Mô tả phòng</h3>
-                <p><?php echo $room['describe_room'] ?></p>
+                <p><?php echo $kindRoom['describe'] ?></p>
                 <div class="room-uitil">
                         <div><i class="fa-solid fa-wifi"></i>
                             <p>Wife</p></i>
@@ -96,7 +97,8 @@
                 </div>
                 <div class="comment-list">
                     <h3>Comment về sản phẩm</h3>
-                    <?php if(!empty($comments)) :?>
+                    <?php 
+                    if(!empty($comments)) :?>
                         <?php foreach ($comments as $comment): ?>
                             <div class="comment-item">
                                 <h5 class="username">
@@ -115,7 +117,7 @@
                 <form action="" method="POST" enctype="multipart/form-data">
                     <div class="price">
                         <h4>Bắt Đầu Đặt</h4>
-                        <input readonly="False"  name="price_room" id="price_room" value="<?=$room['price_room']?>" class="price-item">
+                        <input readonly="False"  name="price_room" id="price_room" value="<?=$kindRoom['price']."VNĐ"?>" class="price-item">
                         <div class="night">VND OVERNIGHT</div>
                     </div>
                     <div class="date-room">
@@ -137,28 +139,28 @@
                         <button type="submit" class="book-room">Đặt Phòng Ngay</button>
                     </div>
                     <?php
-                        date_default_timezone_set("Asia/Ho_Chi_Minh");
-                        if (isset($_POST['date_star']) && isset($_POST['date_end']) && isset($_POST['price_room'])) {
+                        // date_default_timezone_set("Asia/Ho_Chi_Minh");
+                        // if (isset($_POST['date_star']) && isset($_POST['date_end']) && isset($_POST['price_room'])) {
 
-                            if (isset($_SESSION['user_id'])) {
-                                $user_id = $_SESSION['user_id'];
-                                $room_book = new BookRoom(); 
-                                $date_star = $_POST['date_star'];
-                                $date_end = $_POST['date_end'];
-                                $date_star = date($date_star);
-                                $date_end = date ( "Y-m-d" , strtotime ('+'.$date_end.'day' , strtotime($date_star)));                           
-                                $price_room = $_POST['price_room'];
-                                $room_book->add($id_room,$user_id,$date_star,$date_end, $price_room,);
-                            } else{
+                        //     if (isset($_SESSION['user_id'])) {
+                        //         $user_id = $_SESSION['user_id'];
+                        //         $room_book = new BookRoom(); 
+                        //         $date_star = $_POST['date_star'];
+                        //         $date_end = $_POST['date_end'];
+                        //         $date_star = date($date_star);
+                        //         $date_end = date ( "Y-m-d" , strtotime ('+'.$date_end.'day' , strtotime($date_star)));                           
+                        //         $price_room = $_POST['price_room'];
+                        //         $room_book->add($id_room,$user_id,$date_star,$date_end, $price_room,);
+                        //     } else{
                     ?>
                                 <script>                                    
-                                        if (confirm('Đăng nhập để tiếp tục')) {
-                                        window.location="view/dangnhap.php";                                        
-                                    }
+                                    //     if (confirm('Đăng nhập để tiếp tục')) {
+                                    //     window.location="view/dangnhap.php";                                        
+                                    // }
                                 </script>
                     <?php
-                            }                            
-                        }
+                        //     }                            
+                        // }
                     ?>
                 </form>
             </div>
@@ -167,15 +169,15 @@
         <div class="room-next">
             <div class="zoom next">
                 <img src="./view/img/deitail/next.jpg" alt="">
-                <p>Outdoor Pool</p>
+                <p>Phòng Tổng Thống</p>
             </div>
             <div class="zoom next">
                 <img src="./view/img/deitail/next2.jpg" alt="">
-                <p>Bathroom</p>
+                <p>Phòng Nhóm 2</p>
             </div>
             <div class="zoom next">
                 <img src="./view/img/deitail/next3.png" alt="">
-                <p>View Hotel</p>
+                <p>Phòng Thủ Tướng</p>
             </div>
         </div>
         <?php
@@ -195,18 +197,18 @@
     <?php require('./view/footer.php'); ?>
 
     <script>
-    let price = document.getElementById('price_room');
+        let price = document.getElementById('price_room');
         document.getElementById('add').onclick = function (event) {
             event.preventDefault()
             let number_room = document.getElementById('number_room')           
-            number_room.value++
+            number_room.value++;
             price.value = <?=$room['price_room']?> * number_room.value;
         }
         document.getElementById('reduce').onclick = function (event) {
             event.preventDefault()
             let number_room = document.getElementById('number_room')
             if (number_room.value > 1) {
-                number_room.value--
+                number_room.value--;
                 price.value = price.value - <?=$room['price_room']?>;
             }
         }
--- controller/kindRoom/kindRoom.php
+++ controller/kindRoom/kindRoom.php
@@ -1,9 +1,9 @@
 <?php
-require('../../model/connect.php');
-$sql = "SELECT * FROM `kindRoom`";
-$show = $connect->query($sql);
-$show->execute();
-$list = $show->fetchAll();
+    require('../../model/connect.php');
+    $sql = "SELECT * FROM `kindRoom`";
+    $show = $connect->query($sql);
+    $show->execute();
+    $list = $show->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,8 +52,11 @@ $list = $show->fetchAll();
                 <table>
                     <thead>
                         <tr>
-                            <th>Kind Of Room ID</th>
+                        <th>Kind Of Room ID</th>
                             <th>Kind Of Room</th>
+                            <th>Price</th>
+                            <th>Describe</th>
+                            <th>Image</th>
                             <th>Delete and Update</th>
                         </tr>
                     </thead>
@@ -62,8 +65,11 @@ $list = $show->fetchAll();
                         foreach ($list as $item) {
                         ?>
                             <tr>
-                                <td><?php echo $item['kind_of_room_id'] ?></td>
-                                <td><?php echo $item['kind_of_room'] ?></td>
+                            <td><?php echo $item['kind_of_room_id'] ?></td>
+                            <td><?php echo $item['kind_of_room'] ?></td>
+                            <td><?php echo $item['price'] ?></td>
+                            <td><?php echo $item['describe'] ?></td>
+                            <td><img src="<?php echo $item['image'] ?>" width="100px" height="100px"></td>
                                 <td>
                                     <a onclick="return confirm('Do you want delete?')" href="delete.php?id=<?php echo $item['kind_of_room_id'] ?>">Delete</a>
                                     <a href="update.php?id=<?php echo $item['kind_of_room_id'] ?>">Update</a>
