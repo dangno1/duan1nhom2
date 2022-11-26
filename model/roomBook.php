4f84951af6212286e3f8b471db267c4f7423056f
phanvanloi <106456389+phanvanloi@users.noreply.github.com>
2022-11-26 23:04:35 +0700

    sua_bookedroom_loi

--- model/roomBook.php
+++ model/roomBook.php
@@ -4,13 +4,13 @@ require('connect.php');
 
 class BookRoom
 {
-    public function add($room_id,$user_id,$start_time,$end_time,$total_money)
+    public function add($kind_of_room_id,$user_id,$start_time,$end_time,$amount,$total_money)
     {
-        $sql = "INSERT INTO roombooked(room_id, user_id, start_time, end_time, total_money) 
-                VALUES ('$room_id','$user_id','$start_time','$end_time','$total_money')";
+        $sql = "INSERT INTO roombooked(kind_of_room_id, user_id, start_time, end_time,amount, total_money, status) 
+                VALUES ('$kind_of_room_id','$user_id','$start_time','$end_time','$amount','$total_money','Chưa Duyệt')";
         $result = $GLOBALS['connect']->query($sql);
         if ($result) {
             echo '<div class="susbok">Đặt phòng thành công</div>';
-        }
+        } else echo '<div class="susbok">Đặt phòng thành công</div>';
     }
 }
